1a261c33dafc638831977f453ecb9080d42a5050
Yves Godart <yves@MacBook-Pro-de-Yves.local>
2019-02-14 01:43:08 +0100

    night

--- src/App.js
+++ src/App.js
@@ -2,65 +2,80 @@ import React from "react";
 //import { Router } from './components'
 //import routes from './pages/routes'
 import "./styles/index.less";
-import { Layout } from "antd";
-import { Button } from "antd";
+import { Layout, Col, Row } from "antd";
+import { Button, Progress } from "antd";
 import "antd/dist/antd.css";
 import path from "path";
-import ListFiles from './components/ListFiles'
+import ListFiles from "./components/ListFiles";
 
 const { dialog } = require("electron").remote;
-
+let percent=0;
 const { Header, Footer, Sider, Content } = Layout;
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      defaultDir: path.resolve("./")
+      defaultDir: path.resolve("./"),
+      percent: 0
     };
-    console.log(this.state)
+    console.log(this.state);
   }
 
   componentDidMount() {
     //console.clear();
   }
 
-  updateState= (state) => {
+  updateState = state => {
+    this.setState(
+      {
+        ...state
+      },
+      () => {
+        console.log("updateState", this.state);
+      }
+    );
+  };
+
+  updateProgress = (percent) =>{
+    console.log(percent)
     this.setState({
-      ...state
-    },() => {
-      console.log('updateState',this.state)
-    })
+      percent: percent
+    });
   }
-  
+
   clickOpen = e => {
     e.preventDefault();
     dialog.showOpenDialog({ properties: ["openDirectory"] }, dirname => {
-      this.updateState({defaultDir: dirname[0]});
-      
+      this.updateState({ defaultDir: dirname[0] });
     });
   };
   render() {
-    const {defaultDir} = this.state;
+    const { defaultDir } = this.state;
     return (
       <div id="app">
-        <Layout>
-          <Header>
-          <Button
-                type="primary"
-                icon="download"
-                onClick={e => this.clickOpen(e)}
-              >
-                Open.....
-              </Button>
-          </Header>
-          <Layout>
-            <Sider style={{color: '#fff'}}>
-              <ListFiles defaultDir={defaultDir}></ListFiles>
-            </Sider>
-            <Content>Content</Content>
-          </Layout>
-          <Footer>Footer</Footer>
-        </Layout>
+    <Progress percent={this.state.percent}></Progress>
+        <Row>
+          <Col span={24}>Titre</Col>
+        </Row>
+        <Row>
+          <Col span={6}>
+            <Button
+              type="primary"
+              icon="download"
+              onClick={e => this.clickOpen(e)}
+            >
+              Open.....
+            </Button>
+            <h2>Liste</h2>
+                  <div style={{height:"90vh", overflow:"auto"}}>
+                    <ListFiles defaultDir={defaultDir} onProgress={this.updateProgress}/>
+                  </div>
+                
+          </Col>
+          <Col span={18}>
+            <h2>Détail</h2>
+          </Col>
+        </Row>
       </div>
     );
   }
--- src/components/ListFiles.js
+++ src/components/ListFiles.js
@@ -1,26 +1,70 @@
 import React, { Component } from "react";
+import {List} from "antd";
+import fs from 'fs';
+import path from 'path';
+//import nodeID3v2 from 'node-id3v2.4';
+const NodeID3 = Promise.resolve(require('node-id3'));
 
 class ListFiles extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      defaultDir: props.defaultDir
+      defaultDir: props.defaultDir,
+      files:[]
     };
     console.log(props.defaultDir);
+  }
+
+  getTags = (file) => {
+    return new Promise((resolve, reject) => {
+      let filename = path.resolve(this.state.defaultDir, file);
+      resolve({})
+    })
+  }
+
+  fillList = (dir) => {
+    return new Promise((resolve, reject) => {
+      fs.readdir(dir,(err,filelist)=> {
+        if (filelist) {
+          
+          resolve(filelist.map((file,i) => {
+            this.getTags(file)
+            .then(json => {
+              this.props.onProgress(i)
+              return i + file
+            })
+            
+          }));
+        }
+      })
+    })
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.defaultDir !== this.props.defaultDir) {
       this.setState({
         defaultDir: this.props.defaultDir
+      }, () => {
+        this.fillList(this.state.defaultDir)
+        .then((files)=>{
+          this.setState({files})
+        });
       });
     }
   }
   render() {
+    const list = this.state.files.map((file,i) => {
+      return (<li key={i}>{file}</li>)
+    })
+    //console.log(list)
     return (
-      <div>
-        <h1 style={{ color: "#eee" }}>ListFiles</h1>
-      </div>
+      <List size="small"    
+        bordered
+        dataSource={this.state.files}
+        renderItem={item => (<List.Item style={{cursor:"pointer"}} onClick={(e) =>{alert(item)}}>{item}</List.Item>)}>
+        
+      </List>
+      
     );
   }
 }
